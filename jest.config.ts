cfc8f20a1e3dd14d1608d14b363dc3f82b735c56
DeborahLuna <deborahdlmb@gmail.com>
2026-06-03 19:52:14 -0300

    hotfix: adicionar jest.config

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/tests"],
+  testMatch: ["**/*.test.ts"],
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.json" }],
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/server.ts",
+    "!src/config/database.ts",
+  ],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov"],
+};
+
+export default config;
